843ea15f984b39265a51f7802cb544768090bcd0
Daniel Sochor <danielsochor@gmail.com>
2019-08-07 16:36:43 -0500

    progress on login

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,126 +1,163 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./LoginPage.css";
 import M from "materialize-css";
 
 
-function Login_SignUp(){
+function Login_SignUp() {
+    const [loginType, setLoginType] = useState(LOGIN_TYPE.signin);
 
     const [emailVal, setEmailVal] = useState('');
+
+    //**THIS SHOLldnt be needed */
+    //const handleLoginToggle = (type) => {
+    //if (type === LOGIN_TYPE.signin) {
+    //setModalType(AUTH_MODAL_TYPES.signin);
+    //setChangeTypeBtnText(changeTypeBtnTextValues.signin);
+    //} else if (type === LOGIN_TYPE.signup) {
+    // setModalType(AUTH_MODAL_TYPES.signup);
+    //setChangeTypeBtnText(changeTypeBtnTextValues.signup);
+    //}
+
+    //setErrorMessage('');
+    //setModalIsOpen(!modalIsOpen);
+    //}
 
     const handleEmailChange = (event) => {
         // @TODO implement live validation
         setEmailVal(event.target.value);
-      }
-    
+    }
 
 
-//class Login_SignUp extends Component {
-  //  componentDidMount() {
+    //class Login_SignUp extends Component {
+    //  componentDidMount() {
     //    M.AutoInit();
     //}
 
     //state = {
-     //   username: "",
-      //  password: ""
+    //   username: "",
+    //  password: ""
     //}
 
     //handleInputChange = event => {
-       // const { name, value } = event.target
+    // const { name, value } = event.target
 
-       // this.setState({
-      //      [name]: value
-       // })
+    // this.setState({
+    //      [name]: value
+    // })
     //};
 
-    //render() {
-        return (
-            <div>
-                <div className="center white-text">
-                    <h4>HealthMate</h4>
-                </div>
-                <div className="loginContainer container ">
-                    <div className="loginDiv row center ">
-                        <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Login</a>
-                    </div>
-                    <div className="signUpDiv row center">
-                        <a className="waves-effect waves-light btn modal-trigger" href="#modal2">Sign Up</a>
-                    </div>
-                </div>
-                {/* Login Modal */}
+    const authSubmit = (event) => {
+        event.preventDefault();
+    
+        if (modalType === AUTH_MODAL_TYPES.signin) {
+          let signinObj = {
+            email_address: emailVal,
+            password: passwordVal
+          };
+    
+          Auth.sendSigninRequest(signinObj);
+        } else if (modalType === AUTH_MODAL_TYPES.signup) {
+          let signupObj = {
+            first_name: firstNameVal,
+            last_name: lastNameVal,
+            alias: usernameVal,
+            email: emailVal,
+            password: passwordVal,
+            password_confirm: confirmPasswordVal 
+          };
+    
+          Auth.sendSignupRequest(signupObj);
+        }
+      }
 
-                <div id="modal1" className="modal">
-                    <div className="modal-content">
-                        <h4>Login</h4>
-                        <form className="col s12">
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="loginUsername" type="text" className="validate" />
-                                    <label htmlFor="loginUsername">Username</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="loginPassword" type="password" className="validate" />
-                                    <label htmlFor="loginPassword">Password</label>
-                                </div>
-                            </div>
-                            <div className="row center">
-                                <button className="btn waves-effect waves-light" type="submit" name="action" id="loginBtn">Login
+
+
+    const generateFormContents = () => {
+        if (loginType === LOGIN_TYPE.signin) {
+            return (
+                //<h4>Login</h4>
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="loginUsername" type="text" className="validate" />
+                            <label htmlFor="loginUsername">Username</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="loginPassword" type="password" className="validate" />
+                            <label htmlFor="loginPassword">Password</label>
+                        </div>
+                    </div>
+                    <div className="row center">
+                        <button className="btn waves-effect waves-light" type="submit" name="action" id="loginBtn">Login
                                     <i className="material-icons right">send</i>
-                                </button>
-                            </div>
-                        </form>
+                        </button>
                     </div>
-                    <div className="modal-footer">
-                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">X</a>
+                </form>
+            );
+        } else {
+            return (
+                //<h4>Sign Up</h4>
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="email" type="email" className="validate" value={emailVal} onChange={handleEmailChange} />
+                            <label htmlFor="email">Email</label>
+                        </div>
                     </div>
-                </div>
-
-                {/* Sign Up Modal */}
-
-                <div id="modal2" className="modal modal-fixed-footer">
-                    <div className="modal-content">
-                        <h4>Sign Up</h4>
-                        <form className="col s12">
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate" value={emailVal} onChange={handleEmailChange}/>
-                                    <label htmlFor="email">Email</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="signUpUsername" type="text" className="validate" />
-                                    <label htmlFor="signUpUsername">Username</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="signUpPassword" type="password" className="validate" />
-                                    <label htmlFor="signUpPassword">Password</label>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="input-field col s12">
-                                    <input id="confirmPassword" type="password" className="validate" />
-                                    <label htmlFor="confirmPassword">Confirm Password</label>
-                                </div>
-                            </div>
-                            <div className="row center">
-                                <button className="btn waves-effect waves-light" type="submit" name="action" id="signUpBtn">Sign Up
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="signUpUsername" type="text" className="validate" />
+                            <label htmlFor="signUpUsername">Username</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="signUpPassword" type="password" className="validate" />
+                            <label htmlFor="signUpPassword">Password</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input id="confirmPassword" type="password" className="validate" />
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                        </div>
+                    </div>
+                    <div className="row center">
+                        <button className="btn waves-effect waves-light" type="submit" name="action" id="signUpBtn">Sign Up
                                     <i className="material-icons right">send</i>
-                                </button>
-                            </div>
-                        </form>
+                        </button>
                     </div>
-                    <div className="modal-footer">
-                        <a href="#!" className="modal-close waves-effect waves-green btn-flat">X</a>
-                    </div>
-                </div>
-            </div>
-        )
+                </form>
+            )
+        }
     }
-//}
+
+    return (
+        <div>
+            <div className="center white-text">
+                <h4>HealthMate</h4>
+            </div>
+            <div className="loginContainer container ">
+                {generateFormContents()}
+                {/*
+                <div className="loginDiv row center ">
+                    <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Login</a>
+                    <button type='button' className='btn btn-primary mx-2' onClick={() => toggleModal(AUTH_MODAL_TYPES.signin)}>Log In</button>
+                </div>
+                <div className="signUpDiv row center">
+                    <a className="waves-effect waves-light btn modal-trigger" href="#modal2">Sign Up</a>
+                </div>
+*/}
+                <button type='submit' className='btn btn-primary' onClick={authSubmit}>Submit</button>
+            </div>
+        </div>
+    )
+}
+
+
 
 export default Login_SignUp;
+
 
