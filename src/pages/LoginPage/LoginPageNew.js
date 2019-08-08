df8af70bb49eca2028cff4846ea3d7da159d9c3e
Daniel Sochor <danielsochor@gmail.com>
2019-08-08 16:29:16 -0500

    further progress on login modal

--- src/pages/LoginPage/LoginPageNew.js
+++ src/pages/LoginPage/LoginPageNew.js
@@ -3,28 +3,47 @@ import "./LoginPage.css";
 import Modal from 'react-modal';
 import Auth from '../../utilities/auth';
 
+const changeTypeBtnTextValues = {
+    signin: 'Don\'t have an account?',
+    signup: 'Already have an account?'
+  };
+
 //tasks
-//check if session exisits
+//check if session exists
 //if not
-//show modal with login or signup
-//press either button
+//show login first and then let them switch to sign up
 //repopulates modal with different information
 
 function Login_SignUp() {
 
+    const [modalType, setModalType] = useState(login);
+    const [modalIsOpen, setModalIsOpen] = useState(true);
+
     useEffect(() => {
         Auth.checkForExistingSession();
     }, []);
+    //for now assume no session
+
+    const toggleModalType = () => {
+        let newModalType = modalType === signin ? signup : signin;
+        let newChangeBtnText = modalType === signin ? changeTypeBtnTextValues.signup : changeTypeBtnTextValues.signin;
+        setModalType(newModalType);
+        setChangeTypeBtnText(newChangeBtnText);
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
 
     const authSubmit = (event) => {
         event.preventDefault();
-        if (modalType === AUTH_MODAL_TYPES.signin) {
+        if (modalType === login) {
             let signinObj = {
                 email_address: emailVal,
                 password: passwordVal
             };
             Auth.sendSigninRequest(signinObj);
-        } else if (modalType === AUTH_MODAL_TYPES.signup) {
+        } else if (modalType === signup) {
             let signupObj = {
                 first_name: firstNameVal
             };
@@ -33,12 +52,12 @@ function Login_SignUp() {
     }
 
     const generateFormContents = () => {
-        if (modalType === AUTH_MODAL_TYPES.signin) {
+        if (modalType === login) {
             return (
                 <div className='modal-body'>
                 </div>
             );
-        } else if (modalType === AUTH_MODAL_TYPES.signup) {
+        } else if (modalType === signup) {
             return (
                 <div className='modal-body'>
                 </div>
@@ -54,7 +73,12 @@ function Login_SignUp() {
             onRequestClose={closeModal}
             contentLabel='Login Modal'>
             <form>
+                <h5 className='modal-title'>{modalType}</h5>
                 {generateFormContents()}
+                <button type='button' className='btn btn-link' onClick={toggleModalType}>{changeTypeBtnText}</button>
+                <div className='modal-footer'>
+                    <button type='submit' className='btn btn-primary' onClick={authSubmit}>Submit</button>
+                </div>
             </form>
         </Modal>
     )
