ae32693d5c121bf2fa971b0423451cbc5671b1bd
Justin German <justin.german23@gmail.com>
2019-08-10 10:47:11 -0500

    user log page done

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Navbar from "../../components/Navbar/Navbar";
 import FloatingActionButton from "../../components/FloatingButton/Fab"
+import ChallengePage from "../../components/ChallengePage/ChallengePage"
 
 
 function HomePage() {
     return (
         
+            // <div>
+            //     <Navbar />
+            //     <div className="container">
+            //         <div className="row" style={{textAlign:"center"}}>
+            //             <h4>
+            //                 Welcome, User("this is a placeholder")
+            //             </h4>
+            //             <h5>
+            //                 Click on the button to start a new challenge!
+            //             </h5>
+            //             <FloatingActionButton />
+            //         </div>
+            //     </div>
+            // </div>
+
             <div>
-                <Navbar />
-                <div className="container">
-                    <div className="row" style={{textAlign:"center"}}>
-                        <h4>
-                            Welcome, User("this is a placeholder")
-                        </h4>
-                        <h5>
-                            Click on the button to start a new challenge!
-                        </h5>
-                        <FloatingActionButton />
-                    </div>
-                </div>
+                <ChallengePage />
             </div>
         
     );
