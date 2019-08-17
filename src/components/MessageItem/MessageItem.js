d78e773e77f8ec49e90d843d4d64b062a0ad3518
Justin German <justin.german23@gmail.com>
2019-08-17 11:54:36 -0500

    message board working with toggle hide

--- src/components/MessageItem/MessageItem.js
+++ src/components/MessageItem/MessageItem.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import M from "materialize-css";
+
+
+
+
 
 class MessageItem extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            class: 'truncate',
+            show: false
+        }
+    }
+
+
+    
+
+
     render() {
-     
+        // collection-item avatar z-depth-2
         return (
-            <li className="collection-item avatar z-depth-2">
-            <img src="https://chridd.nfshost.com/avatar/hd/black.png" alt="" className="circle"/>
-            <p><span className="left">{this.props.message_author}:</span><br/>
-            <span className="left">{this.props.message_body}</span><br/>
-            <span className="right">{this.props.createdAt}</span><br/>
-            </p>
-          </li>
-        
+
+            <li onClick={() => {this.props.handleClick(this.props.index)}} >
+                <div className="collapsible-header">
+                    <img src="https://chridd.nfshost.com/avatar/hd/black.png" alt="" className="circle" style={{ height: "50px" }} />
+                    <p style={{ marginLeft: "10px" }}><span className="left">{this.props.message_author}:</span></p>
+                    <p id="truncateBody" className={this.props.showMe ? "truncate hide": "truncate"} style={{ marginLeft: "20px" }}>{this.props.message_body}</p>
+                </div>
+                <div className="collapsible-body"><span className="">{this.props.message_body}</span><br />
+                    <span className="right">{this.props.createdAt}</span><br />
+                </div>
+            </li>
+
         );
     }
 }
 
-            
+
 export default MessageItem;
